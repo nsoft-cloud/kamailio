ae1426db197b189b0d3810ea1c1e36e790a3f8ad
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-03-14 14:08:15 +0100

    tls: OPENSSL_fork_prepare is deprecated in openssl 3.0
    https://www.openssl.org/docs/manmaster/man3/OPENSSL_fork_prepare.html
    
    > void OPENSSL_fork_prepare(void);
    > void OPENSSL_fork_parent(void);
    > void OPENSSL_fork_child(void);
    > [...]
    > These methods are currently unused, and as such, no replacement
    > methods are required or planned.

--- src/modules/tls/tls_mod.c
+++ src/modules/tls/tls_mod.c
@@ -412,7 +412,7 @@ static int mod_init(void)
 	if(sr_tls_event_callback.s==NULL || sr_tls_event_callback.len<=0) {
 		tls_lookup_event_routes();
 	}
-#if OPENSSL_VERSION_NUMBER >= 0x010101000L
+#if OPENSSL_VERSION_NUMBER >= 0x010101000L && OPENSSL_VERSION_NUMBER < 0x030000000L
 	/*
 	 * register the need to be called post-fork of all children
 	 * with the special rank PROC_POSTCHILDINIT
@@ -450,7 +450,7 @@ static int mod_child(int rank)
 									&mod_params, &mod_params) < 0)
 				return -1;
 		}
-#if OPENSSL_VERSION_NUMBER >= 0x010101000L
+#if OPENSSL_VERSION_NUMBER >= 0x010101000L && OPENSSL_VERSION_NUMBER < 0x030000000L
 		if(ksr_tls_init_mode&TLS_MODE_FORK_PREPARE) {
 			OPENSSL_fork_prepare();
 		}
@@ -458,7 +458,7 @@ static int mod_child(int rank)
 		return 0;
 	}
 
-#if OPENSSL_VERSION_NUMBER >= 0x010101000L
+#if OPENSSL_VERSION_NUMBER >= 0x010101000L && OPENSSL_VERSION_NUMBER < 0x030000000L
 	if(ksr_tls_init_mode&TLS_MODE_FORK_PREPARE) {
 		if(rank==PROC_POSTCHILDINIT) {
 			/*
